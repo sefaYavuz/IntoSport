4f918fdf49397e9d368d0a0bd25c0e54b23033b6
Bart Lanen <b.bart.lanen@gmail.com>
2014-10-11 14:36:31 +0200

    klantmodel file

---
+++ Models/KlantModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace IntoSport.Models
+{
+    public class KlantModel
+    {
+
+
+    }
+
+
+    public class RegisterKlantModel
+    {
+        [Required]
+        [Display(Name = "Email adres")]
+        [EmailAddress(ErrorMessage = " The email adres is not valid")]
+        public EmailAddressAttribute 
+
+        [Required]
+        public EmailAddressAttribute
+    }
+}
