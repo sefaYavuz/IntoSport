ac96420ea084282b0d0cf79e224eda78a5e92528
Denzyl Dick <denzyl@live.nl>
2014-10-16 11:09:22 -0700

    Error commitment

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using IntoSport.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+using System.Web.Mvc;
+
+namespace IntoSport.Controllers
+{
+    public class ProductController : DatabaseConnector
+    {
+      
+        public List<Product> MeestVerkochteProducten()
+        {
+            List<Product> producten = new List<Product>();
+            string sql = " SELECT * FROM product";
+            MySqlCommand command = new MySqlCommand();
+           
+        }
+
+    }
+}
