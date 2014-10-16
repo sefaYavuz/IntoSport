deaf628da8d109849bda4a1a383677a824b806e2
sefaYavuz <seff61@gmail.com>
2014-10-16 18:42:53 +0200

    added the ability to add/edit categories

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using IntoSport.Models;
 
 namespace IntoSport.Controllers
 {
@@ -33,6 +34,68 @@ namespace IntoSport.Controllers
         public ActionResult Manager()
         {
             return View();
+        }
+
+        [Authorize(Roles = "beheerder")]
+        public ActionResult Categories()
+        {
+            ViewData.Add("categoriesp", Models.Category.getAllCategories());
+            ViewData.Add("search", "");
+            return View();
+        }
+
+        [Authorize(Roles = "beheerder")]
+        [HttpPost]
+        public ActionResult Categories(String search = "")
+        {
+            ViewData.Add("categoriesp", Models.Category.getAllCategories(search));
+            ViewData.Add("search", search);
+            return View();
+        }
+
+        [Authorize(Roles = "beheerder")]
+        public ActionResult Category(int CategoryID = 0)
+        {
+            Category category = new Category(CategoryID);
+            ViewData.Add("categorie", category);
+            return View("Category");
+        }
+
+        [Authorize(Roles = "beheerder")]
+        public ActionResult DeleteCategory(int CategoryID)
+        {
+            Query q = new Query();
+            q.Delete("categorie");
+            q.Where("id =" + CategoryID);
+            q.Execute();
+            return RedirectToAction("Categories", "Admin");
+        }
+
+        [Authorize(Roles = "beheerder")]
+        [HttpPost]
+        public ActionResult Category(FormCollection collection)
+        {
+            if (!collection["naam"].Equals(""))
+            {
+                Category c = new Category();
+                c.id = int.Parse(collection["id"]);
+                c.naam = collection["naam"];
+                c.parent = new Category(int.Parse(collection["parent.id"]));
+
+                if (c.id != 0)
+                {
+                    c.Update();
+                }
+                else
+                {
+                    c.id = c.Insert();
+                }
+                ViewData.Add("msg", "De wijzigingen zijn succesvol opgeslagen.");
+                Category category = new Category(c.id);
+                ViewData.Add("categorie", category);
+                return View("Category");
+            }
+            return View("categorie?categoryID=" + collection["id"] + "&failed=1");
         }
     }
 }
--- Models/Login.cs
+++ Models/Login.cs
@@ -10,9 +10,11 @@ namespace IntoSport.Models
     public class Login : User
     {
 
-        [Required]
+        [Required(ErrorMessage = "- Email is een verplicht veld!")]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "- Voer een geldige e-mail adres in!")]
+
         public override string email { get; set; }
-        [Required]
+        [Required(ErrorMessage = "- Wachtwoord is een verplicht veld!")]
         public override string wachtwoord { get; set; } 
     }
 }
