320c15a10f7cad6494d2ac49678cfe76fcdda393
Denzyl Dick <denzyl@live.nl>
2014-10-16 11:07:52 -0700

    Product model

---
+++ Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntoSport.Models
+{
+    public class Product 
+    {
+        public int id{get; set;}
+        private int orderregel2_id{get; set;}
+        private int categorie_id{get; set;}
+        private string naam{get; set;}
+        private double prijs{get; set;}
+        private double prijs_oud{get; set;}
+        private double hoeveelheid{get; set;}
+        private string merk{get; set;}
+        private string maat{get; set;}
+        private string afbeelding{get; set;}
+        private string thumbnail{get; set;}
+
+    }
+}
