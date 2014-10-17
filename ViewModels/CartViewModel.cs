e4ed7424df1201a8d5c2182b73bed268d14c6a6d
Bart Lanen <b.bart.lanen@gmail.com>
2014-10-17 16:27:00 +0200

    winkelwagen files van de branch voor de merge

---
+++ ViewModels/CartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IntoSport.ViewModels
+{
+    public class CartViewModel
+    {
+        string[] CartArray { get; set; }
+
+    }
+
+}
