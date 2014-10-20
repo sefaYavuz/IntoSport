b82a6bee3f7c3e79527f50531bf5fbf5c16fbff9
Bart Lanen <b.bart.lanen@gmail.com>
2014-10-21 01:47:57 +0200

    init contact controller

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntoSport3.Controllers
+{
+    public class ContactController : Controller
+    {
+        //
+        // GET: /Contact/
+
+        public ActionResult Index()
+        {
+            return View("Account");
+        }
+
+
+        public ActionResult NoAccount()
+        {
+            return View("NoAccount");
+        }
+
+        public ActionResult Account()
+        {
+            return View("Account");
+        }
+
+    }
+}
