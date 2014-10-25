457c1629bd913ffa1355ad86acbc3d2c8b12c938
Denzyl Dick <denzyl@live.nl>
2014-10-25 12:43:20 -0700

    Commit

--- Global.asax.cs
+++ Global.asax.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
-using System.Web.Optimization;
+//using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace IntoSport3
@@ -21,7 +21,7 @@ namespace IntoSport3
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+          //  BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
