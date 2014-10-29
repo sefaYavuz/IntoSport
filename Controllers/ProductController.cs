137acd32956ba8f0fb021f1f37547852a8ef36c8
Denzyl Dick <denzyl@live.nl>
2014-10-29 15:24:10 -0700

    Added URL categorie support

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -5,67 +5,31 @@ using System.Linq;
 using System.Web;
 using MySql.Data.MySqlClient;
 using System.Web.Mvc;
+using IntoSport.Helpers;
 
 namespace IntoSport.Controllers
 {
     public class ProductController : Controller
     {
-        public ActionResult Index()
+        ProductHelper productHelper = new ProductHelper();
+
+        public ActionResult Index(int categorie)
         {
+            ViewData.Add("product", productHelper.getProduct(categorie));
             return View();
+
         }
+    
         
-        public ActionResult Detail()
+        public ActionResult Detail(int id)
         {
+            ViewData.Add("product", new Product(id));
+            ViewData.Add("details", productHelper.getDetails(id));
             return View();
         }
 
 
-        public List<Omzet> MeestVerkochteProducten()
-        {
-            conn.Open();
-            List<Omzet> omzets = new List<Omzet>();
-            //  string sql = " SELECT naam, count(*) as aantal_verkochte, (prijs * count(*)) as omzet FROM product Group By product.id";
-            string sql = "SELECT product.naam, SUM(orderregel.hoeveelheid) as aantal_verkochte,(SUM(product.prijs) * SUM(orderregel.hoeveelheid) )as omzet FROM orderregel JOIN product ON orderregel.product_id = product.id GROUP BY product.id ORDER BY omzet DESC";
-            MySqlCommand command = new MySqlCommand(sql, conn);
-            MySqlDataReader reader = command.ExecuteReader();
-            while (reader.Read() != false)
-            {
-
-                omzets.Add(
-                    new Omzet(
-                        reader.GetString("naam"),
-                        reader.GetInt32("aantal_verkochte"),
-                        reader.GetDouble("omzet")
-                        )
-                       );
-            }
-            return omzets;
-        }
-
-
-        public List<Omzet> MinstVerkochteProducten()
-        {
-            conn.Open();
-            List<Omzet> omzets = new List<Omzet>();
-            //  string sql = " SELECT naam, count(*) as aantal_verkochte, (prijs * count(*)) as omzet FROM product Group By product.id";
-            string sql = "SELECT product.naam, SUM(orderregel.hoeveelheid) as aantal_verkochte,(SUM(product.prijs) * SUM(orderregel.hoeveelheid) )as omzet FROM orderregel JOIN product ON orderregel.product_id = product.id GROUP BY product.id ORDER BY omzet ASC";
-            MySqlCommand command = new MySqlCommand(sql, conn);
-            MySqlDataReader reader = command.ExecuteReader();
-            while (reader.Read() != false)
-            {
-
-                omzets.Add(
-                    new Omzet(
-                        reader.GetString("naam"),
-                        reader.GetInt32("aantal_verkochte"),
-                        reader.GetDouble("omzet")
-                        )
-                       );
-            }
-            return omzets;
-        }
-
+     
 
 
     }
