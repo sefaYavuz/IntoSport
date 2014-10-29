ebc045b5814ba4c987dbc2393414bffe3f6d797e
Bart Lanen <b.bart.lanen@gmail.com>
2014-10-29 14:34:43 +0100

    OrderDBController

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -15,13 +15,30 @@ namespace IntoSport.Controllers
 
         public ActionResult Index()
         {
-            if (Request.Cookies["cart"] != null)
-            {
-                String[] cart = CartHelper.getItems(Request.Cookies["cart"].Values["cart"]);
-                ViewData["cart"] = cart;
-            }
+			if (Request.Cookies ["cart"] != null) {
+				String[] cart = CartHelper.getItems (Request.Cookies ["cart"].Values["cart"]);
+				return View();
+			} else {
+				return View();
+
+			}
             return View();
         }
+
+		[HttpPost]
+		public ActionResult Index()
+		{
+			if(Request.Cookies[FormsAuthentication.FormsCookieName] != null){
+				HttpCookie authCookie = Request.Cookies[FormsAuthentication.FormsCookieName];//.ASPXAUTH
+				FormsAuthenticationTicket authTicket = FormsAuthentication.Decrypt(authCookie.Value);
+				string id = authTicket.Name;
+				OrderDBController ODBC = new OrderDBController();
+			}
+			else
+			{
+				return Redirect("Register");
+			}
+		}
 
         public ActionResult addToCart(int ID, int Quantity)
         {
@@ -30,12 +47,14 @@ namespace IntoSport.Controllers
                 HttpCookie cart = new HttpCookie("cart");
                 cart.Value = CartHelper.addItem(ID, Quantity);
                 Response.Cookies.Add(cart);
+				return RedirectToAction("Index");
 
             }
             else
             {
                 Response.Cookies["cart"].Value = CartHelper.addItem(Request.Cookies["cart"].Value, ID, Quantity);
                 Response.Cookies["cart"].Expires = DateTime.Now.AddDays(1);
+				return RedirectToAction("Index");
             }
 
             return View();
---
+++ Helpers/OrderDBController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using IntoSport.Models;
+using System.Web.Mvc;
+using System.Collections.Generic;
+
+namespace IntoSport
+{
+	public class OrderDBController : DatabaseConnector
+	{
+		public OrderDBController ()
+		{
+
+		}
+
+		public void makeOrder(string id, string cart)
+		{
+			Query q = new Query();
+			q.Select("MAX(id)");
+			q.From("Bestelling");
+			q.Limit ("1");
+			int maxOrderPlus = 0;
+
+			foreach (Dictionary<string, object> order in q.Execute()) {
+
+				if ((int)order ["id"] == null) {
+					maxOrderPlus = 0;
+				} else {
+					maxOrderPlus = (int)order ["id"] + 1;
+				}
+
+			}
+
+			Query q2 = new Query();
+			DateTime saveNow = DateTime.Now;
+
+			Dictionary<string, object> bestelling = new Dictionary<string,object> ();
+			bestelling.Add ("user_id", id);
+			bestelling.Add ("status", "in_behandeling");
+			bestelling.Add ("datum", saveNow.ToString ());
+			bestelling.Add ("korting", 0);
+
+			q2.Execute("bestelling", bestelling);
+
+
+			foreach(string value in CartHelper.getItems(cart)){
+
+				Query q3 = new Query ();
+
+				string[] product = value.Split (',');
+
+				Dictionary<string, object> order_regel = new Dictionary<string,object> ();
+				order_regel.Add ("product_id", product [0]);
+				order_regel.Add ("bestelling_id", maxOrderPlus.ToString();)
+				order_regel.Add("hoeveelheid", product[1]);
+
+				q3.Execute("order_regel", order_regel);
+
+
+			}
+
+
+
+
+		}
+	}
+}
+
