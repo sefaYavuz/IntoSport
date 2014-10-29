297c2acbe0f9c8d28ca4b0097380b773737466f4
Denzyl Dick <denzyl@live.nl>
2014-10-29 15:21:40 -0700

    Product Helper

---
+++ Helpers/ProductHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IntoSport.Models;
+using MySql.Data.MySqlClient;
+
+namespace IntoSport.Helpers
+{
+    public class ProductHelper : DatabaseConnector
+    {
+        public List<Product> getProduct(int Categorie)
+        {
+            conn.Open();
+            List<Product> productList = new List<Product>();
+            string sql = "SELECT * FROM categorie JOIN product_categorie ON categorie.id= product_categorie.categorie_id JOIN product ON product_categorie.product_id = product.id WHERE categorie.id = ?id GROUP BY product.id";
+            MySqlCommand command = new MySqlCommand(sql,conn);
+            MySqlParameter id = new MySqlParameter("?id", MySqlDbType.Int32);
+            command.Parameters.Add(id).Value = Categorie;
+            MySqlDataReader reader = command.ExecuteReader();
+        
+            while(reader.Read())
+            {
+                productList.Add(new Product(reader.GetInt32("product_id")));
+            }  return productList;
+
+        }
+
+        public List<KeyValuePair<string,string>> getDetails(int product_id)
+        {
+         
+            List<KeyValuePair<string, string>> detailsList = new List<KeyValuePair<string, string>>();
+            conn.Open();
+            string sql = " select naam,waarde from detail join detail_product on detail.id = detail_product.detail_id join detail_waarde on detail_waarde.detail_id = detail_product.detail_id where product_id =?id";
+            MySqlCommand command = new MySqlCommand(sql, conn);
+            MySqlParameter id = new MySqlParameter("?id", MySqlDbType.Int32);
+            command.Parameters.Add(id).Value = product_id;
+            MySqlDataReader reader = command.ExecuteReader();
+            while(reader.Read())
+            {
+              
+                detailsList.Add( new KeyValuePair<string, string>(reader.GetString("naam"),reader.GetString("waarde")));
+            }
+
+
+            return detailsList;
+
+        }
+
+    }
+}
