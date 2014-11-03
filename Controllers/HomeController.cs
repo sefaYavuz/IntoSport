b89af16b3491309b1bfd1a5f91495d7e93dcfd49
Denzyl Dick <denzyl@live.nl>
2014-11-02 16:14:24 -0800

    add viewdate kortingproducten

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,17 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using IntoSport.Models;
+using IntoSport.Helpers;
 
 namespace IntoSport.Controllers
 {
     public class HomeController : Controller
     {
+        
         //
         // GET: /Home/
 
         public ActionResult Index()
         {
             ViewData.Add("getAllProducts", Product.getAllProducts("", "8", "INNER", "product_categorie AS pc ON product.id = pc.product_id"));
+            ViewData.Add("kortingProducten",new ProductHelper().getKortingen());
             return View();
         }
 
