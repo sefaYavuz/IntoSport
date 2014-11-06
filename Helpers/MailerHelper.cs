3980b2adeec5b737a5a8a7143a4edf826f3ccf4a
Denzyl Dick <denzyl@live.nl>
2014-11-06 05:45:35 -0800

    Try catch

--- Helpers/MailerHelper.cs
+++ Helpers/MailerHelper.cs
@@ -9,20 +9,26 @@ namespace IntoSport.Helpers
     public class MailerHelper
     {
         public MailerHelper(String body, String subject,IntoSport.Models.User user)
-        { 
-     
-            MailMessage mail = new MailMessage("IntoSport@hotmail.com", user.email);
-            SmtpClient client = new SmtpClient();
-            client.Port = 25;
-            client.DeliveryMethod = SmtpDeliveryMethod.Network;
-     
-            client.UseDefaultCredentials = false;
-            client.Credentials = new System.Net.NetworkCredential("IntoSport@hotmail.com", "Werkgroep6C");
-            client.EnableSsl = true;
-            client.Host = "smtp.live.com";
-            mail.Subject = subject;
-            mail.Body = body;
-            client.Send(mail);
+        {
+            try
+            {
+                MailMessage mail = new MailMessage("IntoSport@hotmail.com", user.email);
+                SmtpClient client = new SmtpClient();
+                client.Port = 25;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+                client.UseDefaultCredentials = false;
+                client.Credentials = new System.Net.NetworkCredential("IntoSport@hotmail.com", "Werkgroep6C");
+                client.EnableSsl = true;
+                client.Host = "smtp.live.com";
+                mail.Subject = subject;
+                mail.Body = body;
+                client.Send(mail);
+            }catch(Exception e)
+            {
+
+            }
+           
         }
     }
 }
