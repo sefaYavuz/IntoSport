ecc3dce2007cb292bedb778ce09b9dba13c6e844
Denzyl Dick <denzyl@live.nl>
2014-11-14 07:58:40 -0800

    Add to cart

---
+++ Models/CartProduct.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IntoSport.Models
+{
+    public class CartProduct
+    {
+       
+        int ID { get; set; }
+        [Range(1, Int32.MaxValue)]
+        public int Quantity { get; set; }
+        public List<DetailWaarde> DetailWaardeList { get; set; }
+
+        public CartProduct(){
+            DetailWaardeList = new List<DetailWaarde>();
+
+    }
+    }
+
+  
+
+
+}
